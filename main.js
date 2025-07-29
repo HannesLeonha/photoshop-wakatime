4a951d004b208fe360c4798e74739eba20bf21a1
Hannes Leonhartsberger <hannes.leonhartsberger@gmail.com>
2025-07-29 19:02:57 +0200

    Added wakatime icons

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
+// Initialize Plugin
 const { entrypoints } = require("uxp");
 
-  showAlert = () => {
-    alert("This is an alert message");
-  }
+showAlert = () => {
+  alert("This is an alert message");
+}
 
-  entrypoints.setup({
-    commands: {
-      showAlert,
-    },
-    panels: {
-      vanilla: {
-        show(node ) {
-        }
+entrypoints.setup({
+  panels: {
+    vanilla: {
+      show(node ) {
       }
     }
-  });
+  }
+});
 
+// Panel
 function showLayerNames() {
-    const app = require("photoshop").app;
-    const allLayers = app.activeDocument.layers;
-    const allLayerNames = allLayers.map(layer => layer.name);
-    const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-    document.getElementById("layers").innerHTML = `
-      <ul>${
-        sortedNames.map(name => `<li>${name}</li>`).join("")
-      }</ul>`;
+  const app = require("photoshop").app;
+  const allLayers = app.activeDocument.layers;
+  const allLayerNames = allLayers.map(layer => layer.name);
+  const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
+  document.getElementById("layers").innerHTML = `
+    <ul>${
+      sortedNames.map(name => `<li>${name}</li>`).join("")
+    }</ul>`;
+  console.log("wee woo");
 }
 
 document.getElementById("btnPopulate").addEventListener("click", showLayerNames);
