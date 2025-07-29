3079902af4ff86757de2b2c80a895bb65b99122e
Hannes Leonhartsberger <hannes.leonhartsberger@gmail.com>
2025-07-29 23:08:05 +0200

    Added ui design

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
+const { entrypoints, shell } = require("uxp");
+const photoshop = require("photoshop");
+const app = photoshop.app;
+
 // Initialize Plugin
-const { entrypoints } = require("uxp");
-
-showAlert = () => {
-  alert("This is an alert message");
-}
-
 entrypoints.setup({
   panels: {
     vanilla: {
@@ -15,16 +13,6 @@ entrypoints.setup({
 });
 
 // Panel
-function showLayerNames() {
-  const app = require("photoshop").app;
-  const allLayers = app.activeDocument.layers;
-  const allLayerNames = allLayers.map(layer => layer.name);
-  const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-  document.getElementById("layers").innerHTML = `
-    <ul>${
-      sortedNames.map(name => `<li>${name}</li>`).join("")
-    }</ul>`;
-  console.log("wee woo");
-}
-
-document.getElementById("btnPopulate").addEventListener("click", showLayerNames);
+document.getElementById("openWebsite").onclick = () => {
+  shell.openExternal("https://wakatime.com/dashboard");
+};
